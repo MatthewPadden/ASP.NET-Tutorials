7ce1ada8d5b81abbc897a408d1f0b3ab5a985806
Matthew Padden <paddenmatthew@live.ie>
2015-02-27 16:29:47 +0000

    Started filters

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        /* In-built Authorization filter. It has two public properties Users and Roles with are
+         * string comma-separated lists of the users and roles in the system. When we use the
+         * AuthorizeAttribute we can supply users and/or roles to check against. */
+        [Authorize(Users = "admin")]
+        //[CustomAuth(true)] // when set to false, we will be given the login page
+        public string Index()
+        {
+            return "This is the Index action of the Home controller";
+        }
+    }
+}
---
+++ Filters/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    /* This custom filter is a Authorization filter as it inherits from AuthorizeAttribute which
+     * implements IAuthorizationFilter. We could have our own implemntation of IAuthorizationFilter
+     * but that would mean we would have to write our own security code which is dangerous. Here
+     * we use MVC's own implementation and add in our own custom code which lets us use MVC's security
+     * code. */
+
+    /* Types of filters and order they are ran:
+     * 1. Authentication    -   IAuthenticationFilter   -   Before action method, can be run again
+     * 2. Authorization     -   IAuthorizationFilter    -   Before action method, can be run again
+     * 3. Action            -   IActionFilter           -   Before and after action method
+     * 4. Result            -   IResultFilter           -   Before and after the action result is executed
+     * 5. Exception         -   IExceptionHandler       -   Only if another filter, the action method or
+     *                                                      the action result throws an exception */
+
+    /// <summary>
+    /// Does not allow access if the request is local
+    /// </summary>
+    public class CustomAuthAttribute : AuthorizeAttribute
+    {
+        private bool localAllowed;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="allowedParam">True if local requests are permitted</param>
+        public CustomAuthAttribute(bool allowedParam)
+        {
+            localAllowed = allowedParam;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.IsLocal)
+                return localAllowed;
+            else
+                return true;
+        }
+    }
+}
