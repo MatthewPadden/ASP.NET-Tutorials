e7075e021aef5e01b90048cf44aac48b851eb495
Matthew Padden <paddenmatthew@live.ie>
2015-03-10 14:44:22 +0000

    Completed exception filters

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using Filters.Infrastructure;
 
 namespace Filters.Controllers
@@ -24,6 +25,33 @@ namespace Filters.Controllers
         public string List()
         {
             return "This is the List action on the Home controller";
+        }
+
+        /* Without adding an Exception filter to this action method, if a user gets here with the
+         * id segment variable < 100, we will get the yellow creen of death. By adding an Exception
+         * filter we can handle the exception ourselves and avoid that. */
+
+        [RangeException]
+        public string RangeTest(int id)
+        {
+            if (id > 100)
+                return string.Format("The id value is: {0}", id);
+            else
+                throw new ArgumentOutOfRangeException("id", id, "");
+        }
+
+        /* This uses the built in Exception filter called HandleError. As we can see, we tell it which
+         * exception to look out for and then what view to return, we can also specify the layout page
+         * to use. The model that will be passed to the view will be a HandleErrorInfo object which is
+         * a wrapper around the exception. */
+
+        [HandleError(ExceptionType = typeof(ArgumentOutOfRangeException), View = "RangeErrorWithHandleError")]
+        public string RangeTestWithBHandleError(int id)
+        {
+            if (id > 100)
+                return string.Format("The id value is: {0}", id);
+            else
+                throw new ArgumentOutOfRangeException("id", id, "");
         }
     }
 }
---
+++ Filters/Infrastructure/RangeExceptionAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    /* Exception filters are only run if an unhandled exception has been thrown when invoking an action
+     * method. This can occur from the following locations:
+     *  1. Another kind of filter
+     *  2. The action method itself
+     *  3. When the action result is executed */
+
+    /* Exception filters must implement IExceptionFilter. In order for a .NET class to be treated as
+     * an MVC filter, it must implement IMvcFilter which we get from FilterAttribute and which provides
+     * some useful basic features such as handling the default order in which filters are processed */
+
+    public class RangeExceptionAttribute : FilterAttribute, IExceptionFilter
+    {
+        /* OnException is called when an unhandled exception arises */
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            // check if the exception is already handled before checking the type of exception
+            if (!filterContext.ExceptionHandled && filterContext.Exception is ArgumentOutOfRangeException)
+            {
+                //filterContext.Result = new RedirectResult("~/Content/RangeErrorPage.html");
+
+                // instead of going to a static page we go to a view which can give contextual information
+                int val = (int)(((ArgumentOutOfRangeException)filterContext.Exception).ActualValue);
+                filterContext.Result = new ViewResult
+                {
+                    ViewName = "RangeError",
+                    ViewData = new ViewDataDictionary<int>(val)
+                };
+
+                // set the exception to handled to prevent the exception being raised again
+                filterContext.ExceptionHandled = true;
+            }
+        }
+    }
+}
